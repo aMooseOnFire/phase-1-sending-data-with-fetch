d10371a47756a6ab2a40ff806582cf12d07dfe90
amos <amosjshinkle@gmail.com>
2022-12-20 08:24:43 -0500

    commit tree a716bef9f41938c07e51c1836b5cf43d44636a1c

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData (userName, userEmail) {
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          name: userName,
+          email: userEmail,
+        }),
+      };
+      
+      return fetch("http://localhost:3000/users", configurationObject)
+        .then(resp => resp.json())
+        .then(object => {
+            console.log(object)
+            const h1 = document.createElement('h1')
+            h1.textContent = object.id
+            document.body.appendChild(h1)
+        }) 
+        .catch(function (error) {
+            const h2 = document.createElement('h2')
+            h2.textContent = "Unauthorized Access"
+            document.body.appendChild(h2)
+        })
+
+}
